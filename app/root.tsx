09647e96f5855c498da79fdf6603fbb8200d3e0c
Kalianto Wong <kalianto@gmail.com>
2024-06-16 12:44:25 +1000

    added tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,11 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-} from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts } from '@remix-run/react';
+import styles from './tailwind.css?url';
 
 export default function App() {
   return (
     <html>
       <head>
-        <link
-          rel="icon"
-          href="data:image/x-icon;base64,AA"
-        />
+        <link rel='icon' href='data:image/x-icon;base64,AA' />
         <Meta />
         <Links />
       </head>
